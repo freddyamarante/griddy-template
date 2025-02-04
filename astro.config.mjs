bd4cd82584371e9ed61fe5c350109c3d249ea95f
freddy-seelo <freddy@seelo.ch>
2025-02-04 18:06:51 -0400

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: 'jcexkvnt',
+      dataset: 'production',
+      useCdn: false,
+      studioBasePath: '/studio'
+    }), 
+    react()
+  ],
+  output: 'server'
+});
