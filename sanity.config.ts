93157fc2471a32c7479fa88a640c0c48744ecae2
freddy-seelo <freddy@seelo.ch>
2025-02-05 17:08:24 -0400

    feat: add sanity commerce plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,9 +3,32 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {colorInput} from '@sanity/color-input'
+import {sanityCommerce, type SanityCommercePluginConfig,} from '@commercelayer/sanity-plugin-commerce';
 
 import {schemaTypes} from './src/studio/schemas'
 import {structure} from './src/studio/structure'
+
+const sanityCommerceConfig: SanityCommercePluginConfig = {
+  productLabel: 'Coffee',
+  variantLabel: 'Coffee Variant',
+  taxonomyLabel: 'Category System',
+  taxonLabel: 'Category',
+  productAttributes: [{ name: 'origin', title: 'Origin', type: 'string' }],
+  variantAttributes: [
+    {
+      name: 'roastLevel',
+      title: 'Roast Level',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Light', value: 'light' },
+          { title: 'Medium', value: 'medium' },
+          { title: 'Dark', value: 'dark' },
+        ],
+      },
+    },
+  ],
+};
 
 export default defineConfig({
   name: 'default',
@@ -18,6 +41,7 @@ export default defineConfig({
     structureTool({structure}), 
     visionTool(), 
     colorInput(),
+    sanityCommerce(sanityCommerceConfig)
   ],
 
   schema: {
